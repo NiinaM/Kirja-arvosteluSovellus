335619befd8ed3ba81036e6022b3893d49946da0
Niina-Maria Takalo <niina-maria.takalo@helsinki.fi>
2020-01-24 02:23:26 +0200

    lisäsin listausmahdollisuuden kirjoille ja mahdollisuuden merkata kirja luetuksi

--- kirjaarvostelusovellus/application/books/views.py
+++ kirjaarvostelusovellus/application/books/views.py
@@ -1,10 +1,23 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.books.models import Book
+
+@app.route("/books", methods=["GET"])
+def books_index():
+    return render_template("books/list.html", books = Book.query.all())
 
 @app.route("/books/new/")
 def books_form():
     return render_template("books/new.html")
+  
+@app.route("/books/<book_id>/", methods=["POST"])
+def books_set_read(book_id):
+
+    t = Book.query.get(book_id)
+    t.read = True
+    db.session().commit()
+  
+    return redirect(url_for("books_index"))
 
 @app.route("/books/", methods=["POST"]) #tehty POST-pyyntö lisää uuden tehtävän pyynnössä lähetetyn lomakkeen perusteella
 def books_create():
@@ -12,4 +25,5 @@ def books_create():
 
     db.session().add(t)
     db.session().commit()
-    return "hello world!"
+
+    return redirect(url_for("books_index")) #uudelleenohjataan sivulle, missä tehtävät listataan
