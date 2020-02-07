28381dd8aca9f399ec9ab1e39cb55f9fe0020d94
Niina-Maria Takalo <niina-maria.takalo@helsinki.fi>
2020-02-07 15:27:51 +0200

    pieni muutos run.py

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-from application import app
-
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+from application import app     #lataa moduulista application muuttuja app
 
 if __name__ == '__main__':
     app.run(debug=True)
